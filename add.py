70906ac35ace52e1fe402a4f0f0cc1f0e04d9275
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-07-19 12:04:12 -0600

    upload screenshots and add.py

---
+++ add.py
@@ -0,0 +1,7 @@
+monday_steps = int(input("How many steps did you take on Monday?"))
+tuesday_steps = int(input("How many steps did you take on Tuesday?"))
+
+total_steps = monday_steps + tuesday_steps
+
+print("You have taken ", f"{total_steps:,}", " steps so far this week. Keep it up!")
+
